004afd86064bfe04eb54a8fcc33dc13088e09c90
Joshua-004 <newscollege816@gmail.com>
2026-09-13 20:44:26 -0400

    TTT2025W-20 A message from Joshua

--- GitTraining2026Session1c/GitTraining2026Session1c.cpp
+++ GitTraining2026Session1c/GitTraining2026Session1c.cpp
@@ -9,6 +9,8 @@ int main()
     cout << "Hello World!\n";
 	cout << "Hello world! for git training session 1c\n";
 	cout << "Hello from matt" << endl;
+	cout << "I am outside on campus at this moment, and I am not a resident" << endl;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
