52bfbf60163993b1263ac38cdfc38e94b271d35c
PercyCho <percycho13@gmail.com>
2026-09-13 20:48:04 -0400

    TTT2025W-20 - Joseph commit

--- GitTraining2026Session1c/GitTraining2026Session1c.cpp
+++ GitTraining2026Session1c/GitTraining2026Session1c.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "This is Joseph!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
